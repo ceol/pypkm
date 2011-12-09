a874f0f3e3cc26fdf4234f98efa662ac5437c9f5
Patrick Jacobs <ceolwulf@gmail.com>
2011-12-09 00:10:35 -0500

    added base setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PyPKM',
+      version='0.0',
+      description='PKM File Manipulation',
+      author='Patrick Jacobs',
+      author_email='ceolwulf@gmail.com',
+      url='http://bitbucket.org/ceol/pypkm',
+      packages=['pypkm'],
+     )
