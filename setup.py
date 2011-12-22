8eb18d0c395f098ad0b373c2f35dd8214ff130b9
Patrick Jacobs <ceolwulf@gmail.com>
2011-12-22 17:58:52 -0500

    import error handling, fixed package_data

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(name='PyPKM',
+setup(name='pypkm',
       version='0.0',
       description='PKM File Manipulation',
       author='Patrick Jacobs',
       author_email='ceolwulf@gmail.com',
+      keywords='pkm pokemon',
       url='http://bitbucket.org/ceol/pypkm',
       packages=['pypkm'],
-      package_data=['pypkm': ['db/*.db',],],
+      package_data={'pypkm': ['db/*.db']},
      )
