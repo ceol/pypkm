2eae4d5aff258451bed7c7119e6d81ef89cc49c1
Patrick Jacobs <ceolwulf@gmail.com>
2011-12-24 01:26:55 -0500

    Pkm and PkmCore classes

---
+++ pypkm/pkm/__init__.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+
+"""PyPKM: Easy PKM file manipulation.
+
+This utility serves as a friendly way to create, edit, encrypt, decrypt, and
+convert PKM files. Its goal is to faciliate the creation of more end-user
+tools and allow developers to work with PKM files.
+
+Example usage:
+    >>> from pypkm.pkm import Pkm
+    >>> pkm = Pkm().load(gen=4, path='gengar.pkm')
+    >>> pkm.move1
+    95
+    >>> pkm.level = 100
+    >>> pkm.save()
+    gengar_new.pkm
+    >>> pkm.level = 50
+    >>> pkm.save('gengar_50.pkm')
+    gengar_50.pkm
+
+For instructions on how to set a specific attribute, refer to that
+attribute's function in the Pkm class.
+
+The encryption and decryption functions (including checksum and shuffle) are
+taken from the pycrypto/crypto module created by Stephen Anthony Uy
+<tsanth@iname.com>. Somehow, somewhere I came across this module, and it's
+been an extremely valuable tool for encrypting and decrypting PKM files to
+send over the GTS.
+
+Knowledge of the PKM file structure comes from the awesome people at
+ProjectPokemon <http://projectpokemon.org/>, both on the forums and in their
+IRC channel.
+"""
+
+__author__ = "Patrick Jacobs <ceolwulf@gmail.com>"
+
+from pypkm.pkm.core import PkmCore
+
+class Pkm(PkmCore):
+    "Wrapper class for all PKM file manipulation."
+    pass
---
+++ pypkm/pkm/core.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+from pypkm.pkm.attr import PkmAttrMapper
+
+class PkmCore(PkmAttrMapper):
+    "Parent class for the Pkm class."
+    pass
