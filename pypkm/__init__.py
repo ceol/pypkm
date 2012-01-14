bc6ddd101b77bfd4fc9d00b9ede5c6806317d9a2
Patrick Jacobs <ceolwulf@gmail.com>
2012-01-13 19:22:48 -0500

    added empty pkm.py; base __init__.py

---
+++ pypkm/__init__.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+"""PyPKM - Easy PKM File Manipulation
+
+"""
+
+__author__ = 'Patrick Jacobs <ceolwulf@gmail.com>'
+#__version__ = '0.4'
+
+def load(gen, data):
+    """Load PKM data.
+
+    Keyword arguments:
+    gen (int) -- the file's game generation
+    data (str) -- the file's binary data
+    """
+    pass
+
+def new(gen):
+    """Create a new PKM file.
+
+    Keyword arguments:
+    gen (int) -- the file's game generation
+    """
+    pass
