1cfdc35eaf6f548c398d4747644ad03f1c55f447
Patrick Jacobs <ceolwulf@gmail.com>
2012-01-15 17:56:16 -0500

    added safe unicode and str funcs

--- pypkm/util.py
+++ pypkm/util.py
@@ -9,3 +9,20 @@ def Swapped(subcon):
         decoder = lambda buf: buf[::-1],
         resizer = lambda length: length
     )
+
+def safe_unicode(obj, *args):
+    """ return the unicode representation of obj """
+    try:
+        return unicode(obj, *args)
+    except UnicodeDecodeError:
+        # obj is byte string
+        ascii_text = str(obj).encode('string_escape')
+        return unicode(ascii_text)
+
+def safe_str(obj):
+    """ return the byte string representation of obj """
+    try:
+        return str(obj)
+    except UnicodeEncodeError:
+        # obj is unicode
+        return unicode(obj).encode('unicode_escape')
