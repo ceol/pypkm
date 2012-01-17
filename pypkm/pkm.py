2f8fe37a4b595e6577abaea82de4db5d6f41114b
Patrick Jacobs <ceolwulf@gmail.com>
2012-01-16 23:46:28 -0500

    added toparty() to gen 4 and 5

--- pypkm/pkm.py
+++ pypkm/pkm.py
@@ -9,6 +9,8 @@ __author__ = 'Patrick Jacobs <ceolwulf@gmail.com>'
 
 from pypkm.structs import g4pkm, g5pkm
 from pypkm.crypto import encrypt, decrypt
+from pypkm.sqlite import get_level, get_nature, get_basestats
+from pypkm.util import calcstat
 
 class BasePkm(object):
 
@@ -48,6 +50,40 @@ class Gen4Pkm(BasePkm):
             raise ValueError('Unsupported PKM file length: expected 136 or 236, received {}'.format(len(data)))
         
         self._load(strc, data)
+    
+    def toparty(self):
+        # even if it's already a party file, we should process it
+        data = self.tostring()[:136]
+        # create empty data to load into Struct
+        data = ''.join([data, '\x00' * 100])
+        new_pkm = Gen4Pkm(data)
+
+        new_pkm.level = get_level(pokemon_id=new_pkm.id, exp=new_pkm.exp)
+
+        nature = get_nature(new_pkm.pv % 25)
+        base_stats = get_basestats(pokemon_id=new_pkm.id)
+
+        new_pkm.stats.current_hp = calcstat(iv=new_pkm.ivs.hp, ev=new_pkm.evs.hp, base=base_stats[0],
+                              level=new_pkm.level, nature_stat=None)
+        new_pkm.stats.max_hp = new_pkm.stats.current_hp
+        new_pkm.stats.attack = calcstat(iv=new_pkm.ivs.attack, ev=new_pkm.evs.attack, base=base_stats[1],
+                            level=new_pkm.level, nature_stat=nature[2])
+        new_pkm.stats.defense = calcstat(iv=new_pkm.ivs.defense, ev=new_pkm.evs.defense, base=base_stats[2],
+                            level=new_pkm.level, nature_stat=nature[3])
+        new_pkm.stats.speed = calcstat(iv=new_pkm.ivs.speed, ev=new_pkm.evs.speed, base=base_stats[3],
+                            level=new_pkm.level, nature_stat=nature[4])
+        new_pkm.stats.spattack = calcstat(iv=new_pkm.ivs.spattack, ev=new_pkm.evs.spattack, base=base_stats[4],
+                            level=new_pkm.level, nature_stat=nature[5])
+        new_pkm.stats.spdefense = calcstat(iv=new_pkm.ivs.spdefense, ev=new_pkm.evs.spdefense, base=base_stats[5],
+                            level=new_pkm.level, nature_stat=nature[6])
+        
+        return new_pkm
+
+    def togts(self):
+        pass
+
+    def togen5(self):
+        pass
 
 class Gen5Pkm(BasePkm):
     
@@ -63,3 +99,34 @@ class Gen5Pkm(BasePkm):
             raise ValueError('Unsupported PKM file length: expected 136 or 220, received {}'.format(len(data)))
         
         self._load(strc, data)
+    
+    def toparty(self):
+        # even if it's already a party file, we should process it
+        data = self.tostring()[:136]
+        # create empty data to load into Struct
+        data = ''.join([data, '\x00' * 84])
+        new_pkm = Gen5Pkm(data)
+
+        new_pkm.level = get_level(pokemon_id=new_pkm.id, exp=new_pkm.exp)
+
+        nature = get_nature(new_pkm.pv % 25)
+        base_stats = get_basestats(pokemon_id=new_pkm.id)
+
+        new_pkm.stats.current_hp = calcstat(iv=new_pkm.ivs.hp, ev=new_pkm.evs.hp, base=base_stats[0],
+                              level=new_pkm.level, nature_stat=None)
+        new_pkm.stats.max_hp = new_pkm.stats.current_hp
+        new_pkm.stats.attack = calcstat(iv=new_pkm.ivs.attack, ev=new_pkm.evs.attack, base=base_stats[1],
+                            level=new_pkm.level, nature_stat=nature[2])
+        new_pkm.stats.defense = calcstat(iv=new_pkm.ivs.defense, ev=new_pkm.evs.defense, base=base_stats[2],
+                            level=new_pkm.level, nature_stat=nature[3])
+        new_pkm.stats.speed = calcstat(iv=new_pkm.ivs.speed, ev=new_pkm.evs.speed, base=base_stats[3],
+                            level=new_pkm.level, nature_stat=nature[4])
+        new_pkm.stats.spattack = calcstat(iv=new_pkm.ivs.spattack, ev=new_pkm.evs.spattack, base=base_stats[4],
+                            level=new_pkm.level, nature_stat=nature[5])
+        new_pkm.stats.spdefense = calcstat(iv=new_pkm.ivs.spdefense, ev=new_pkm.evs.spdefense, base=base_stats[5],
+                            level=new_pkm.level, nature_stat=nature[6])
+        
+        return new_pkm
+    
+    def togts(self):
+        pass
